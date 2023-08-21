594f2bd6e54929ce2fc6d5a57b94e7c8fd6a59cc
pnaskardev <pnaskardev@gmail.com>
2023-08-21 16:51:17 +0530

    added user model

---
+++ user/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework import serializers
+
+from . models import User
+
+class UserSerializer(serializers.ModelSerializer):
+
+    password_1 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email',
+                  'password', 'password_1']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password_1 = attrs.get('password_1')
+
+        if password != password_1:
+            raise serializers.ValidationError("Passwords should match")
+        return attrs
+
+    def create(self, validated_data):
+        # print(validated_data)
+        validated_data.pop('password_1')
+        # print(validated_data)
+        # return super().create(validated_data)
+        password = validated_data.pop('password')
+        user = super().create(validated_data)
+        if password is not None:
+            user.set_password(password)
+        user.save()
+        return user
